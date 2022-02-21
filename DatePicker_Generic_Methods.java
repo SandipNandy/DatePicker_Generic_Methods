0be6fc6239bc8e2152cbb08520d189f5cd1a08e5
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-21 19:58:18 +0530

    2nd commit
    dir changed

--- DatePicker_Generic_Methods.java
+++ DatePicker_Generic_Methods.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DatePicker_Generic_Methods {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		
