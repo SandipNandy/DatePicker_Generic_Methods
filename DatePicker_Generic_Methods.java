2beeb75c620b57363479964a521892814b30bf75
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-21 19:57:49 +0530

    1st commit

---
+++ DatePicker_Generic_Methods.java
@@ -0,0 +1,48 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DatePicker_Generic_Methods {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.path2usa.com/travel-companions");
+		driver.manage().timeouts().implicitlyWait(6,TimeUnit.SECONDS);
+		driver.findElement(By.id("travel_date")).click();
+		
+		Generic_Methods(driver);
+		
+	}
+	
+	
+	static void Generic_Methods(WebDriver driver){
+		
+		while(!driver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText().contains("June"))
+		{
+		driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+		}
+
+		List<WebElement> dates= driver.findElements(By.className("day"));
+		//Grab common attribute//Put into list and iterate
+		int count=driver.findElements(By.className("day")).size();
+		
+		for(int i=0;i<count;i++)
+		{
+		String text=driver.findElements(By.className("day")).get(i).getText();
+		if(text.equalsIgnoreCase("21"))
+		{
+		driver.findElements(By.className("day")).get(i).click();
+		break;
+		}
+		}
+	}
+
+}
